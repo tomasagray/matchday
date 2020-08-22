21e2a33411b30fcef0c69f8247c47213ab456b18
Tomás Gray <tomas.a.gray@gmail.com>
2020-08-21 22:32:54 -0700

    Add file server user login tool to admin dashboard

---
+++ src/main/resources/public/admin/js/login.gui.js
@@ -0,0 +1,81 @@
+
+$setUserLoggedIn = function($user) {
+  // Set check box checked
+  $user
+    .children('input[type="checkbox"]')
+    .first()
+    .prop("checked", true);
+  // Set data
+  $user.data("logged-in", true);
+  // Set CSS classes
+  $user.removeClass('logged-out').addClass('logged-in');
+}
+
+$setUserLoggedOut = function($user) {
+  // Set check box unchecked
+  $user
+    .children('input[type="checkbox"]')
+    .first()
+    .prop("checked", false);
+  $user.data("logged-in", false);
+  // Set CSS classes
+  $user.removeClass('logged-in').addClass('logged-out');
+}
+
+$(document).on('click', '.server', function() {
+  console.log("Loading Users for Server");
+
+  // Load data
+  let $serverId = $(this).data("id");
+  $loadServerUsers($serverId);
+  // Set title
+  $('#server-title').html($(this).html());
+  // Set CSS classes
+  $('.server').removeClass("selected");
+  $(this).addClass("selected");
+
+  console.log("Loaded Users for Server:", $serverId);
+});
+
+$(document).on('click', '.user', function() {
+  // Get data
+  let $userId = $(this).data("user-id");
+  let $serverId = $(this).data("server-id");
+  let $loggedIn = $(this).data("logged-in");
+  console.log("Logged in", $loggedIn);
+
+  // Construct data
+  let $user = {
+    "userId": $userId,
+    "serverId": $serverId
+  };
+  // Toggle login
+  if ($loggedIn == true) {
+    // Logout
+    console.log("Logging out", $user);
+    $logoutUser($user, $setUserLoggedOut, $(this));
+
+  } else {
+    // Re-login
+    console.log("Re-logging in", $user);
+    $reloginUser($user, $setUserLoggedIn, $(this));
+  }
+});
+
+$(document).on('click', '#login-button', function() {
+
+  // Get data
+  let $username = $('#username').val();
+  let $password = $('#password').val();
+  let $serverId = $('#server-id').val();
+
+  // Construct user object
+  let $user = {
+    "userName": $username,
+    "password": $password,
+    "serverId": $serverId
+  };
+
+  // Perform login request
+  $loginUser($user);
+});
---
+++ src/main/resources/public/admin/js/login.js
@@ -0,0 +1,227 @@
+const $baseUrl = "http://localhost:8081/file-servers/";
+
+$loadServers = function() {
+  $.ajax({
+    type: "GET",
+    url: $baseUrl + "all",
+    dataType: "json",
+    success: function(data) {
+      console.log("Load servers: Success", data);
+
+      // Get data
+      let $servers = data._embedded.fileservers;
+      // Clear list
+      $('#server-list').html("");
+
+      // Add servers to list
+      $servers.forEach(function(server, index) {
+        let $listItem =
+           '<li class="server" data-id="' +  server.id + '">' +
+             server.title +
+           '</li>';
+        $('#server-list').append($listItem);
+      });
+
+      // Auto-select first server
+      $loadServerUsers($servers[0].id);
+      $('#server-title').text($servers[0].title);
+      $('#server-list').children().first().addClass("selected");
+    },
+    error: function(data) {
+      console.log("ERROR loading servers!", data);
+    }
+  });
+}
+
+$loadServerUsers = function($serverId) {
+
+  // Set hidden form field
+  $('#server-id').val($serverId);
+  // Clear user list
+  $('#user-list').html("");
+
+  $.ajax({
+    type: "GET",
+    url: $baseUrl + "file-server/" + $serverId + "/users",
+    dataType: 'json',
+    success: function(data) {
+      console.log("Load Users", "Success", data);
+
+      // Clear user list
+      $('#user-list').html("");
+      // Get data
+      let $users = data._embedded.users;
+      // Add each user to list
+      $users.forEach(function($user, index) {
+        // Determine if logged in checkbox should be checked
+        let $checked = ($user.loggedIn == true ? 'checked' : '');
+        let $loggedInClass = ($user.loggedIn == true ? ' logged-in' : ' logged-out');
+        $('#user-list').append(
+          '<li class="user' + $loggedInClass + '" ' +
+          'data-user-id="' + $user.id + '" ' +
+          'data-server-id="' + $serverId + '" ' +
+          'data-logged-in="' + $user.loggedIn + '" ' +
+          '>' +
+             $user.username +
+            '<input type="checkbox" class="logged-checkbox" ' +
+              $checked + '/>' +
+          '</li>'
+        );
+      });
+    },
+    error: function(data) {
+      console.log("Load Users", "No users found", data);
+      $('#user-list').html(
+        '<p style="font-style: italic; color: #666">' +
+          'No users for this server' +
+        '</p>'
+      );
+    }
+  });
+}
+
+// Add user
+$loginUser = function($user) {
+
+  $disableLoginButton();
+
+  // Construct URL
+  let $url = $baseUrl + "file-server/" + $user.serverId + "/login";
+
+  // Post login request
+  $.ajax({
+    type: "POST",
+    url: $url,
+    data: JSON.stringify($user),
+    dataType: "json",
+    contentType: "application/json; charset=utf-8"
+  }).done(function(data) {
+    console.log("Success: ", data);
+
+    // Update user table
+    $loadServerUsers($user.serverId);
+
+    $message = {
+      status: 200,
+      responseJSON: data
+    };
+    displayMessage($message);
+
+    $enableLoginButton();
+
+  }).fail(function(data){
+    displayMessage(data);
+    $enableLoginButton();
+  });
+};
+
+$logoutUser = function($user, $callback, $args) {
+
+  // Construct URL
+  let $url = $baseUrl + "file-server/" + $user.serverId + "/logout";
+
+  $.ajax({
+    type: "POST",
+    url: $url,
+    data: JSON.stringify($user),
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function(data) {
+      console.log("User logout SUCCESS", data, $args);
+      // Call callback
+      $callback($args);
+    },
+    error: function(data) {
+      console.log("User logout FAILURE!", data);
+    }
+  }).done(function(data, statusText, xhr) {
+    displayMessage(xhr);
+  });
+}
+
+$reloginUser = function($user, $callback, $args) {
+
+  // Construct URL
+  let $url = $baseUrl + "file-server/" + $user.serverId + "/relogin";
+
+  $.ajax({
+    type: "POST",
+    url: $url,
+    data: JSON.stringify($user),
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function(data) {
+      console.log("Relogin SUCCESS", data, $args);
+      // Call callback
+      $callback($args);
+    },
+    error: function(data) {
+      console.log("Relogin FAILED!!!", data);
+      displayMessage(data);
+    }
+  }).done(function(data, statusText, xhr) {
+    displayMessage(xhr);
+  });
+}
+
+$disableLoginButton = function() {
+
+  // Disable login button
+  $('#login-button').attr('disabled', true);
+  // Show spinner
+  $('#login-spinner').css('display', 'inline');
+
+};
+
+$enableLoginButton = function() {
+console.log("Enabling login button..");
+
+  // Hide spinner
+  $('#login-spinner').css('display', 'none');
+  // Enable login button
+  $('#login-button').attr('disabled', false);
+
+};
+
+// Display message modal
+function displayMessage(data) {
+
+  console.log("Displaying message", data);
+
+  // Determine message type
+  switch (data.status) {
+    case 200:
+      // Remove other class
+      $('#message-container').removeClass('error-message');
+      $('#message-container').addClass('success-message');
+      break;
+    case 400:
+    case 403:
+    case 404:
+    case 415:
+    case 500:
+      // Remove other class
+      $('#message-container').removeClass('success-message');
+      $('#message-container').addClass('error-message');
+      break;
+  }
+
+  // Set message text
+  let $message = data.responseJSON.message;
+  $('#message-text').html($message);
+
+  // Display message
+  $('#message-container').css('display', 'flex');
+  $('#message-container').animate({
+    opacity: 1
+  }, 500)
+  .delay(2500)
+  .animate({
+    opacity: 0
+  }, {
+    duration: 500,
+    complete: function() {
+      $('#message-container').css('display', 'none');
+    }
+  });
+}
