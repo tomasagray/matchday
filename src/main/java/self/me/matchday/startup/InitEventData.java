6cf3421bb1782ef0173cdacacc57354ad41ddca0
Tomás Gray <tomas.a.gray@gmail.com>
2020-08-29 21:30:18 -0700

    Update startup routines; create initial Event data refresh

--- src/main/java/self/me/matchday/startup/InitEventData.java
+++ src/main/java/self/me/matchday/startup/InitEventData.java
@@ -5,24 +5,30 @@
 package self.me.matchday.startup;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import self.me.matchday.api.service.DataSourceService;
 import self.me.matchday.model.SnapshotRequest;
+import self.me.matchday.util.Log;
 
 /**
  * Class to pre-populate database with data from remote Galataman source.
  */
 @Configuration
-public class LoadEventData {
+public class InitEventData {
 
-  private static final String LOG_TAG = "LoadEventData";
+  private static final String LOG_TAG = "InitEventData";
+  private static final String INIT_MSG =
+      "Performing first time data refresh of all data sources using empty SnapshotRequest:\n%s";
 
-//  @Bean
-  CommandLineRunner initEventSources(DataSourceService dataSourceService) {
+
+  @Bean
+  CommandLineRunner initialEventDataLoad(DataSourceService dataSourceService) {
 
     return args -> {
       // Create empty Snapshot request
       final SnapshotRequest snapshotRequest = SnapshotRequest.builder().build();
+      Log.i(LOG_TAG, String.format(INIT_MSG, snapshotRequest));
 
       // Refresh all data sources with default (empty) request
       dataSourceService.refreshDataSources(snapshotRequest);
