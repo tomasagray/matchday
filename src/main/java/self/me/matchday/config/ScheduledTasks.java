5d37bf17f2b9f316c8415781f717675e6571945c
Tomás Gray <tomas.a.gray@gmail.com>
2020-09-09 21:41:32 -0700

    Add video data clean-up scheduled task

--- src/main/java/self/me/matchday/config/ScheduledTasks.java
+++ src/main/java/self/me/matchday/config/ScheduledTasks.java
@@ -19,12 +19,19 @@
 
 package self.me.matchday.config;
 
-import org.jetbrains.annotations.NotNull;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Duration;
+import java.time.Instant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import self.me.matchday.api.service.DataSourceService;
+import self.me.matchday.api.service.PlaylistLocatorService;
+import self.me.matchday.api.service.VideoStreamingService;
 import self.me.matchday.model.SnapshotRequest;
 import self.me.matchday.util.Log;
 
@@ -38,10 +45,20 @@ public class ScheduledTasks {
   private static final String LOG_TAG = "ScheduledTasks";
 
   private final DataSourceService dataSourceService;
+  private final PlaylistLocatorService playlistLocatorService;
+  private final VideoStreamingService videoStreamingService;
+
+  @Value("${scheduled-tasks.cron.video-data-expired-days}")
+  private int videoDataExpiredDays;
 
   @Autowired
-  public ScheduledTasks(@NotNull final DataSourceService dataSourceService) {
+  public ScheduledTasks(final DataSourceService dataSourceService,
+      final PlaylistLocatorService playlistLocatorService,
+      final VideoStreamingService videoStreamingService) {
+
     this.dataSourceService = dataSourceService;
+    this.playlistLocatorService = playlistLocatorService;
+    this.videoStreamingService = videoStreamingService;
   }
 
   @Scheduled(cron = "${scheduled-tasks.cron.refresh-event-data}")
@@ -52,5 +69,42 @@ public class ScheduledTasks {
     final SnapshotRequest snapshotRequest = SnapshotRequest.builder().build();
     // Refresh data sources
     dataSourceService.refreshDataSources(snapshotRequest);
+  }
+
+  @Scheduled(cron = "${scheduled-tasks.cron.prune-video-data}")
+  public void pruneVideoData() {
+
+    Log.i(LOG_TAG, "Pruning video data more than 2 weeks old...");
+
+    // Examine each playlist & related data
+    playlistLocatorService
+        .getAllPlaylistLocators()
+        .forEach(playlistLocator -> {
+          try {
+
+            // Read creation date of playlist file
+            final BasicFileAttributes playlistFileAttributes =
+                Files.readAttributes(playlistLocator.getPlaylistPath(), BasicFileAttributes.class);
+            final Instant creationTime = playlistFileAttributes.creationTime().toInstant();
+
+            final Duration sinceCreation = Duration.between(creationTime, Instant.now());
+            final Duration expiredDays = Duration.ofDays(videoDataExpiredDays);
+
+            if (sinceCreation.compareTo(expiredDays) > 0) {
+              Log.i(LOG_TAG,
+                  String.format("Video data is more than %s old; deleting for %s",
+                      expiredDays, playlistLocator));
+
+              // This video data is expired; delete it
+              videoStreamingService.deleteVideoData(playlistLocator);
+              // Delete playlist locator
+              playlistLocatorService.deletePlaylistLocator(playlistLocator.getPlaylistId());
+            }
+          } catch (IOException e) {
+            Log.e(LOG_TAG, "Error running scheduled delete of video data", e);
+            // Wrap exception
+            throw new RuntimeException(e);
+          }
+        });
   }
 }
