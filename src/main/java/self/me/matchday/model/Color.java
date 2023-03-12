617b76c4f2e8d7133f67f16a3a112f88e0ec0fc8
Tom?s Gray <tomas.a.gray@gmail.com>
2023-03-12 19:54:58 +0000

    Provide default values for fields in Color class

--- src/main/java/self/me/matchday/model/Color.java
+++ src/main/java/self/me/matchday/model/Color.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Embeddable;
+import javax.persistence.Column;import javax.persistence.Embeddable;
 
 /**
  * Wrap Java AWT Color class for storage and serialization
- *
  */
 @Data
 @AllArgsConstructor
@@ -16,10 +15,14 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Color {
 
-    private int red;
-    private int green;
-    private int blue;
-    private int alpha;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int red = 0;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int green = 0;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int blue = 0;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int alpha = 0;
 
     public Color(int red, int green, int blue) {
         this.red = red;
