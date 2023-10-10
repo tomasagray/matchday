23bfda9eaae0b09517fb2e365a7c680d3d551acd
Tom?s Gray <tomas.a.gray@gmail.com>
2023-10-10 04:46:34 +0000

    Fix bug in FFmpegSingleStreamTaskLog

--- src/main/java/self/me/matchday/log/plugin/ffmpeg/FFmpegSingleStreamTaskLog.java
+++ src/main/java/self/me/matchday/log/plugin/ffmpeg/FFmpegSingleStreamTaskLog.java
@@ -1,64 +1,34 @@
 package self.me.matchday.log.plugin.ffmpeg;
 
-import java.nio.file.Path;
-import java.time.Duration;
-import java.time.Instant;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.jetbrains.annotations.NotNull;
 import self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask;
-import self.me.matchday.plugin.io.ffmpeg.FFmpegStreamTask;
 
-// @Aspect
-// todo - re-enable this class, figure out why it's throwing AjcClosure ClassNotFound exception
+@Aspect
 public class FFmpegSingleStreamTaskLog {
 
   private static final Logger logger = LogManager.getLogger(FFmpegSingleStreamTask.class);
 
-  @Around("execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.run(..))")
-  public Object logRunFfmpegStreamTask(@NotNull ProceedingJoinPoint jp) throws Throwable {
-    FFmpegStreamTask task = (FFmpegStreamTask) jp.getTarget();
-    Path playlistPath = task.getPlaylistPath();
-    logger.info("Running FFMPEG stream task to: {} ...", playlistPath);
-    Instant start = Instant.now();
-    Object result = jp.proceed();
-    Instant end = Instant.now();
-    long duration = Duration.between(start, end).getSeconds();
-    logger.debug(
-        "FFMPEG stream execution to: {} took: {}",
-        playlistPath,
-        String.format("%d:%02d:%02d", duration / 3600, (duration % 3600) / 60, (duration % 60)));
-    return result;
-  }
-
-  @Around("execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.kill(..))")
-  public Object logKillFfmpegTask(@NotNull ProceedingJoinPoint jp) throws Throwable {
-    FFmpegStreamTask task = (FFmpegStreamTask) jp.getTarget();
-    logger.info("Killing FFMPEG stream task to: {} ...", task.getPlaylistPath());
-    Boolean killed = (Boolean) jp.proceed();
-    logger.info("FFMPEG stream task successfully killed? {}", killed);
-    return killed;
-  }
-
-  @Around("execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.getArguments(..))")
-  public Object logGetFFmpegStreamTaskCommand(@NotNull ProceedingJoinPoint jp) throws Throwable {
-    Object command = jp.proceed();
-    logger.info("Executing FFMPEG command:\n{}", command);
-    return command;
+  @AfterReturning(
+      value =
+          "execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.getArguments(..))",
+      returning = "args")
+  public void logGetFFmpegStreamTaskCommand(@NotNull Object args) {
+    logger.info("Executing FFMPEG command: {}", args);
   }
 
   @Before("execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.prepareStream(..))")
   public void logPrepareFfmpegStream() {
-    logger.debug("Preparing FFMPEG stream for execution...");
+    logger.debug("Preparing single FFMPEG stream for execution...");
   }
 
-  @Around("execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.getInputArg(..))")
-  public Object logGetFfmpegInputString(@NotNull ProceedingJoinPoint jp) throws Throwable {
-    Object input = jp.proceed();
+  @AfterReturning(
+      value =
+          "execution(* self.me.matchday.plugin.io.ffmpeg.FFmpegSingleStreamTask.getInputArg(..))",
+      returning = "input")
+  public void logGetFfmpegInputString(@NotNull Object input) {
     logger.debug("Sending input to FFMPEG: {}", input);
-    return input;
   }
 }
