2eb14ebfa82a11e027dab7461d55705a26d3a8db
Tom?s Gray <tomas.a.gray@gmail.com>
2024-05-15 15:51:28 +0000

    Remove @Transactional annotation from DataSourceService

--- src/main/java/self/me/matchday/api/service/DataSourceService.java
+++ src/main/java/self/me/matchday/api/service/DataSourceService.java
@@ -22,7 +22,6 @@ package self.me.matchday.api.service;
 import org.hibernate.Hibernate;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import self.me.matchday.db.DataSourceRepository;
 import self.me.matchday.db.PatternKitRepository;
 import self.me.matchday.model.DataSource;
@@ -40,146 +39,142 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
-@Transactional
 public class DataSourceService implements EntityService<DataSource<?>, UUID> {
 
-  private final SnapshotService snapshotService;
-  private final DataSourcePluginService pluginService;
-  private final DataSourceRepository dataSourceRepository;
-  private final PatternKitRepository patternKitRepository;
+    private final SnapshotService snapshotService;
+    private final DataSourcePluginService pluginService;
+    private final DataSourceRepository dataSourceRepository;
+    private final PatternKitRepository patternKitRepository;
 
-  DataSourceService(
-      SnapshotService snapshotService,
-      DataSourceRepository dataSourceRepository,
-      DataSourcePluginService pluginService,
-      PatternKitRepository patternKitRepository) {
-    this.snapshotService = snapshotService;
-    this.dataSourceRepository = dataSourceRepository;
-    this.pluginService = pluginService;
-    this.patternKitRepository = patternKitRepository;
-  }
-
-  /**
-   * Refresh all <b>enabled</b> data sources with the given Snapshot
-   *
-   * @param request Refresh request details
-   * @return The SnapshotRequest, for additional processing
-   */
-  public SnapshotRequest refreshAllDataSources(@NotNull final SnapshotRequest request)
-      throws IOException {
-    for (DataSourcePlugin plugin : pluginService.getEnabledPlugins()) {
-      refreshDataSourcesForPlugin(request, plugin);
+    DataSourceService(
+            SnapshotService snapshotService,
+            DataSourceRepository dataSourceRepository,
+            DataSourcePluginService pluginService,
+            PatternKitRepository patternKitRepository) {
+        this.snapshotService = snapshotService;
+        this.dataSourceRepository = dataSourceRepository;
+        this.pluginService = pluginService;
+        this.patternKitRepository = patternKitRepository;
     }
-    return request;
-  }
 
-  public void refreshDataSourcesForPlugin(
-      @NotNull SnapshotRequest request, @NotNull DataSourcePlugin plugin) throws IOException {
-
-    final List<DataSource<?>> dataSources =
-        dataSourceRepository.findDataSourcesByPluginId(plugin.getPluginId());
-    for (final DataSource<?> dataSource : dataSources) {
-      refreshDataSource(request, dataSource);
+    /**
+     * Refresh all <b>enabled</b> data sources with the given Snapshot
+     *
+     * @param request Refresh request details
+     * @return The SnapshotRequest, for additional processing
+     */
+    public SnapshotRequest refreshAllDataSources(@NotNull final SnapshotRequest request)
+            throws IOException {
+        for (DataSourcePlugin plugin : pluginService.getEnabledPlugins()) {
+            refreshDataSourcesForPlugin(request, plugin);
+        }
+        return request;
     }
-  }
 
-  public <T> void refreshDataSource(
-      @NotNull SnapshotRequest request, @NotNull DataSource<T> dataSource) throws IOException {
-
-    final DataSourcePlugin dataSourcePlugin =
-        pluginService.getEnabledPlugin(dataSource.getPluginId());
-    final Snapshot<T> snapshot = dataSourcePlugin.getSnapshot(request, dataSource);
-    snapshotService.saveSnapshot(snapshot, dataSource.getClazz());
-  }
-
-  @SuppressWarnings("unchecked cast")
-  public <T> void refreshOnUrl(@NotNull URL url) throws IOException {
-
-    Optional<DataSource<?>> dataSourceOptional = findDataSourceForUrl(url);
-    if (dataSourceOptional.isPresent()) {
-      DataSource<T> dataSource = (DataSource<T>) dataSourceOptional.get();
-      DataSourcePlugin plugin = pluginService.getEnabledPlugin(dataSource.getPluginId());
-      Snapshot<T> snapshot = plugin.getUrlSnapshot(url, dataSource);
-      snapshotService.saveSnapshot(snapshot, dataSource.getClazz());
-    } else {
-      throw new IllegalArgumentException("No matching DataSource for URL: " + url);
+    public void refreshDataSourcesForPlugin(
+            @NotNull SnapshotRequest request, @NotNull DataSourcePlugin plugin) throws IOException {
+        final List<DataSource<?>> dataSources =
+                dataSourceRepository.findDataSourcesByPluginId(plugin.getPluginId());
+        for (final DataSource<?> dataSource : dataSources) {
+            refreshDataSource(request, dataSource);
+        }
     }
-  }
 
-  private Optional<DataSource<?>> findDataSourceForUrl(@NotNull URL url) {
-    List<DataSource<?>> dataSources = dataSourceRepository.findAll();
-    for (DataSource<?> dataSource : dataSources) {
-      String sourceHost = dataSource.getBaseUri().toString();
-      if (url.toString().startsWith(sourceHost)) {
-        return Optional.of(dataSource);
-      }
+    public <T> void refreshDataSource(
+            @NotNull SnapshotRequest request, @NotNull DataSource<T> dataSource) throws IOException {
+        final DataSourcePlugin dataSourcePlugin =
+                pluginService.getEnabledPlugin(dataSource.getPluginId());
+        final Snapshot<T> snapshot = dataSourcePlugin.getSnapshot(request, dataSource);
+        snapshotService.saveSnapshot(snapshot, dataSource.getClazz());
     }
-    return Optional.empty();
-  }
 
-  @Override
-  public DataSource<?> initialize(@NotNull DataSource<?> dataSource) {
-    Hibernate.initialize(dataSource);
-    return dataSource;
-  }
-
-  @Override
-  public DataSource<?> save(@NotNull final DataSource<?> dataSource) {
-    pluginService.validateDataSource(dataSource);
-    if (dataSource instanceof final PlaintextDataSource<?> plaintext) {
-      patternKitRepository.saveAll(plaintext.getPatternKits());
+    @SuppressWarnings("unchecked cast")
+    public <T> void refreshOnUrl(@NotNull URL url) throws IOException {
+        Optional<DataSource<?>> dataSourceOptional = findDataSourceForUrl(url);
+        if (dataSourceOptional.isPresent()) {
+            DataSource<T> dataSource = (DataSource<T>) dataSourceOptional.get();
+            DataSourcePlugin plugin = pluginService.getEnabledPlugin(dataSource.getPluginId());
+            Snapshot<T> snapshot = plugin.getUrlSnapshot(url, dataSource);
+            snapshotService.saveSnapshot(snapshot, dataSource.getClazz());
+        } else {
+            throw new IllegalArgumentException("No matching DataSource for URL: " + url);
+        }
     }
-    return dataSourceRepository.save(dataSource);
-  }
 
-  @Override
-  public List<DataSource<?>> saveAll(@NotNull Iterable<? extends DataSource<?>> entities) {
-    return StreamSupport.stream(entities.spliterator(), false)
-        .map(this::save)
-        .collect(Collectors.toList());
-  }
-
-  public List<DataSource<?>> getDataSourcesForPlugin(@NotNull UUID pluginId) {
-    return dataSourceRepository.findDataSourcesByPluginId(pluginId);
-  }
-
-  @Override
-  public Optional<DataSource<?>> fetchById(@NotNull UUID id) {
-    return dataSourceRepository.findById(id);
-  }
-
-  @Override
-  public List<DataSource<?>> fetchAll() {
-    return dataSourceRepository.findAll();
-  }
-
-  @Override
-  public DataSource<?> update(@NotNull final DataSource<?> dataSource) {
-    final UUID dataSourceId = dataSource.getDataSourceId();
-    final Optional<DataSource<?>> sourceOptional = dataSourceRepository.findById(dataSourceId);
-    if (sourceOptional.isPresent()) {
-      pluginService.validateDataSource(dataSource);
-      return dataSourceRepository.saveAndFlush(dataSource);
+    private Optional<DataSource<?>> findDataSourceForUrl(@NotNull URL url) {
+        List<DataSource<?>> dataSources = dataSourceRepository.findAll();
+        for (DataSource<?> dataSource : dataSources) {
+            String sourceHost = dataSource.getBaseUri().toString();
+            if (url.toString().startsWith(sourceHost)) {
+                return Optional.of(dataSource);
+            }
+        }
+        return Optional.empty();
     }
-    // else...
-    throw new IllegalArgumentException(
-        "Attempting to update nonexistent DataSource with ID: " + dataSourceId);
-  }
 
-  @Override
-  public List<DataSource<?>> updateAll(@NotNull Iterable<? extends DataSource<?>> entities) {
-    return StreamSupport.stream(entities.spliterator(), false)
-        .map(this::update)
-        .collect(Collectors.toList());
-  }
+    @Override
+    public DataSource<?> initialize(@NotNull DataSource<?> dataSource) {
+        Hibernate.initialize(dataSource);
+        return dataSource;
+    }
 
-  @Override
-  public void delete(@NotNull UUID dataSourceId) {
-    dataSourceRepository.deleteById(dataSourceId);
-  }
+    @Override
+    public DataSource<?> save(@NotNull final DataSource<?> dataSource) {
+        pluginService.validateDataSource(dataSource);
+        if (dataSource instanceof final PlaintextDataSource<?> plaintext) {
+            patternKitRepository.saveAll(plaintext.getPatternKits());
+        }
+        return dataSourceRepository.save(dataSource);
+    }
 
-  @Override
-  public void deleteAll(@NotNull Iterable<? extends DataSource<?>> entities) {
-    dataSourceRepository.deleteAll(entities);
-  }
+    @Override
+    public List<DataSource<?>> saveAll(@NotNull Iterable<? extends DataSource<?>> entities) {
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(this::save)
+                .collect(Collectors.toList());
+    }
+
+    public List<DataSource<?>> getDataSourcesForPlugin(@NotNull UUID pluginId) {
+        return dataSourceRepository.findDataSourcesByPluginId(pluginId);
+    }
+
+    @Override
+    public Optional<DataSource<?>> fetchById(@NotNull UUID id) {
+        return dataSourceRepository.findById(id);
+    }
+
+    @Override
+    public List<DataSource<?>> fetchAll() {
+        return dataSourceRepository.findAll();
+    }
+
+    @Override
+    public DataSource<?> update(@NotNull final DataSource<?> dataSource) {
+        final UUID dataSourceId = dataSource.getDataSourceId();
+        final Optional<DataSource<?>> sourceOptional = dataSourceRepository.findById(dataSourceId);
+        if (sourceOptional.isPresent()) {
+            pluginService.validateDataSource(dataSource);
+            return dataSourceRepository.saveAndFlush(dataSource);
+        }
+        // else...
+        throw new IllegalArgumentException(
+                "Attempting to update nonexistent DataSource with ID: " + dataSourceId);
+    }
+
+    @Override
+    public List<DataSource<?>> updateAll(@NotNull Iterable<? extends DataSource<?>> entities) {
+        return StreamSupport.stream(entities.spliterator(), false)
+                .map(this::update)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(@NotNull UUID dataSourceId) {
+        dataSourceRepository.deleteById(dataSourceId);
+    }
+
+    @Override
+    public void deleteAll(@NotNull Iterable<? extends DataSource<?>> entities) {
+        dataSourceRepository.deleteAll(entities);
+    }
 }
